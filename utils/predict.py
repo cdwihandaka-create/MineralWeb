63da6c6da6b186f04127766c64a9c2a4a765df54
cdwihandaka <cdwihandaka@example.com>
2026-07-17 00:19:53 +0700

    Convert to TFLite

--- utils/predict.py
+++ utils/predict.py
@@ -1,9 +1,7 @@
-import tensorflow as tf
 import numpy as np
-
-# ==========================================
-# Nama Kelas
-# ==========================================
+from PIL import Image
+import tflite_runtime.interpreter as tflite
+import os
 
 CLASS_NAMES = [
     "Azurite",
@@ -13,44 +11,39 @@ CLASS_NAMES = [
     "Pyrite"
 ]
 
-# ==========================================
-# Load Model
-# ==========================================
-
-import os
-
 MODEL_PATH = os.path.join(
     os.path.dirname(os.path.dirname(__file__)),
-    "MineralCNN_V1.keras"
+    "MineralCNN.tflite"
 )
 
-model = tf.keras.models.load_model(
-    MODEL_PATH,
-    compile=False
-)
+interpreter = tflite.Interpreter(model_path=MODEL_PATH)
+interpreter.allocate_tensors()
 
-# ==========================================
-# Prediksi Gambar
-# ==========================================
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
 
 def predict_image(image):
 
-    # Resize sesuai input model
-    img = image.resize((224, 224))
+    img = image.resize((224,224))
 
-    # PIL -> NumPy
     img = np.array(img, dtype=np.float32)
 
-    # Normalisasi
     img = img / 255.0
 
-    # Tambahkan dimensi batch
     img = np.expand_dims(img, axis=0)
 
-    # Prediksi
-    prediction = model.predict(img, verbose=0)[0]
+    interpreter.set_tensor(
+        input_details[0]['index'],
+        img
+    )
 
-    # Ambil kelas dengan probabilitas terbesar
+    interpreter.invoke()
+
+    prediction = interpreter.get_tensor(
+        output_details[0]['index']
+    )[0]
+
     pred_idx = np.argmax(prediction)
 
     mineral = CLASS_NAMES[pred_idx]
