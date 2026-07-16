eefe9b71b0aa9c9c603d7170a39135c0a349bda7
cdwihandaka-create <cdwihandaka@gmail.com>
2026-07-17 00:28:43 +0700

    Update predict.py

--- utils/predict.py
+++ utils/predict.py
@@ -1,6 +1,6 @@
 import numpy as np
 from PIL import Image
-import tflite_runtime.interpreter as tflite
+import tensorflow as tf
 import os
 
 CLASS_NAMES = [
@@ -16,38 +16,23 @@ MODEL_PATH = os.path.join(
     "MineralCNN.tflite"
 )
 
-interpreter = tflite.Interpreter(model_path=MODEL_PATH)
+interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-
 def predict_image(image):
-
-    img = image.resize((224,224))
-
+    img = image.resize((224, 224))
     img = np.array(img, dtype=np.float32)
-
     img = img / 255.0
-
     img = np.expand_dims(img, axis=0)
 
-    interpreter.set_tensor(
-        input_details[0]['index'],
-        img
-    )
-
+    interpreter.set_tensor(input_details[0]['index'], img)
     interpreter.invoke()
 
-    prediction = interpreter.get_tensor(
-        output_details[0]['index']
-    )[0]
-
+    prediction = interpreter.get_tensor(output_details[0]['index'])[0]
     pred_idx = np.argmax(prediction)
-
     mineral = CLASS_NAMES[pred_idx]
-
     confidence = float(prediction[pred_idx] * 100)
-
     return mineral, confidence, prediction
